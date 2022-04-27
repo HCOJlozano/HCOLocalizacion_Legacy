83896428e1d594d0a1bf42ab227a7ea4a2bbb2c7
omatute-hco <87147956+omatute-hco@users.noreply.github.com>
2022-04-27 16:37:41 -0500

    Ajuste en revalorizacion de terceros

--- src_HCO/T1.B1.Libraries/T1.B1.IvaCosto/IvaCosto.cs
+++ src_HCO/T1.B1.Libraries/T1.B1.IvaCosto/IvaCosto.cs
@@ -437,18 +437,25 @@ namespace T1.B1.IvaCosto
 
         static public string GetValueThird(string cardCode)
         {
-            var thrid = string.Empty;
-            var strSQL = string.Format(Queries.Instance.Queries().Get("GetThirdRelated"), cardCode);
-            var objRecordSet = (Recordset)MainObject.Instance.B1Company.GetBusinessObject(BoObjectTypes.BoRecordset);
-            objRecordSet.DoQuery(strSQL);
-            objRecordSet.MoveFirst();
-
-            if (objRecordSet.RecordCount > 0)
+            try
             {
-                thrid = objRecordSet.Fields.Item("Code").Value.ToString();
-            }
+                var thrid = string.Empty;
+                var strSQL = string.Format(Queries.Instance.Queries().Get("GetThirdRelated"), cardCode);
+                var objRecordSet = (Recordset)MainObject.Instance.B1Company.GetBusinessObject(BoObjectTypes.BoRecordset);
+                objRecordSet.DoQuery(strSQL);
+                objRecordSet.MoveFirst();
 
-            return thrid;
+                if (objRecordSet.RecordCount > 0)
+                {
+                    thrid = objRecordSet.Fields.Item("Code").Value.ToString();
+                }
+
+                return thrid;
+            }
+            catch
+            {
+                return String.Empty;
+            }
         }
 
         public static void CreateRevaluation(bool Debit, Documents oDoc)
@@ -463,6 +470,7 @@ namespace T1.B1.IvaCosto
 
             }
 
+            var thirdToUse = GetValueThird(oDoc.CardCode);
             MaterialRevaluation oRev = (MaterialRevaluation)MainObject.Instance.B1Company.GetBusinessObject(BoObjectTypes.oMaterialRevaluation);
             SalesTaxCodes otaxCode = (SalesTaxCodes)MainObject.Instance.B1Company.GetBusinessObject(BoObjectTypes.oSalesTaxCodes);
             SalesTaxAuthorities Ta = (SalesTaxAuthorities)MainObject.Instance.B1Company.GetBusinessObject(BoObjectTypes.oSalesTaxAuthorities);
@@ -470,7 +478,6 @@ namespace T1.B1.IvaCosto
 
             try
             {
-
                 oRev.RevalType = "M";
                 oRev.DocDate = oDoc.DocDate;
                 oRev.Comments = "Revalorización por IVA Costo Doc: " + oDoc.DocNum;
@@ -500,6 +507,27 @@ namespace T1.B1.IvaCosto
                     if (oRev.Add() != 0)
                     {
                         var msn = MainObject.Instance.B1Company.GetLastErrorDescription();
+                    }
+                    else
+                    {
+                        var lastRevalDocEntry = MainObject.Instance.B1Company.GetNewObjectKey();
+                        var query = string.Format(Queries.Instance.Queries().Get("GetRevaluationJournal"), lastRevalDocEntry);
+                        var record = (Recordset)MainObject.Instance.B1Company.GetBusinessObject(BoObjectTypes.BoRecordset);
+                            record.DoQuery(query);
+                        if( record.RecordCount > 0 )
+                        {
+                            var journal =(JournalEntries) MainObject.Instance.B1Company.GetBusinessObject(BoObjectTypes.oJournalEntries);
+                            if( journal.GetByKey(int.Parse(record.Fields.Item("TransId").Value.ToString())) )
+                            {
+                                for(int i=0; i < journal.Lines.Count; i++)
+                                {
+                                    journal.Lines.SetCurrentLine(i);
+                                    journal.Lines.UserFields.Fields.Item("U_HCO_RELPAR").Value = thirdToUse;
+                                }
+
+                                journal.Update();
+                            }
+                        }
                     }
                 }
             }
